0b9e4aa82b7d19b7ae7e94eefee468f31b227fe0
Kamen Yovchev <kamen.yovchev@wize-soluions.com>
2021-03-01 16:16:35 +0200

    add output of api key

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "contitive_account_name" {
     value = azurerm_cognitive_account.cognitive_account.name
 }
+
+output "cognitive_account_api_key" {
+    value = azurerm_cognitive_account.cognitive_account.primary_access_key
+}
