afc9ede0cba5bf816a3c0bbbf61741491df937da
Kamen Yovchev <kamen.yovchev@wize-soluions.com>
2021-03-10 15:11:30 +0200

    change kind var

--- cognitive-account.tf
+++ cognitive-account.tf
@@ -4,6 +4,5 @@ resource "azurerm_cognitive_account" "cognitive_account" {
   location            = var.location
   resource_group_name = var.resource_group_name
   kind                = var.kind
-  qna_runtime_endpoint = var.qna_runtime_endpoint
   sku_name = var.cs_sku_name
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,7 @@
 variable  name {}
 variable  location {}
 variable resource_group_name {}
-variable "qna_runtime_endpoint" {}
-variable kind {
-    type = string
-    default = "QnAMaker"
-}
+variable kind {}
 variable cs_sku_name {
     type = string
     default = "S0"
